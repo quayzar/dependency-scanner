cc4b151f83e9359706dd9b7e6aad428370f4618b
Ian MacKenzie <ian@quayzar.com>
2019-04-26 23:00:48 -0400

    complete code review

--- scan.py
+++ scan.py
@@ -16,11 +16,11 @@ init(autoreset=True)
 
 # set up default parameters
 PARAMS = {
-    "verbose": False,
-    "boolean": False,
-    "total": 0,
-    "outdated": 0,
-    "vulnerable": 0
+    "verbose":      False,
+    "boolean":      False,
+    "total":        0,
+    "outdated":     0,
+    "vulnerable":   0
 }
 RED = Style.BRIGHT + Fore.RED
 YELLOW = Style.BRIGHT + Fore.YELLOW
@@ -30,15 +30,24 @@ GREEN = Fore.GREEN
 # FUNCTIONS
 #####################################################
 
+def report(msg):
+    """ Output message to console """
+    if not PARAMS['boolean']: # boolean output flag not set so print
+        print(msg)
+
 def throw_error(msg):
     """ Output error message and stop """
-    print(RED + "FATAL ERROR\n{}".format(msg))
+    report(RED + "FATAL ERROR\n{}".format(msg))
     sys.exit()
+
+def make_suffix(field):
+    """ Return appropriate suffix based on quantity """
+    return "y" if PARAMS[field] == 1 else "ies"
 
 def confirm_project_path(args):
     """ confirm project directory """
 
-    # determine which path to confirmgit status
+    # determine which path to confirm
     path = args.path
     if not path: # no user-provided project path so look for self-check flag
         if not args.check: # no self-check flag
@@ -60,26 +69,21 @@ def find_dependencies_list(path):
     """ Locate the dependencies list within the project directory """
 
     # check for common dependency list names in common locations
-    dep_files = ['requirements.txt'] # add other common filenames (e.g. Pipfile)
+    dep_files = ['requirements.txt'] # add other common filenames here (e.g. Pipfile)
     for dep_file in dep_files:
         deps_list = Path(path + '/' + dep_file)
         if deps_list.exists(): # found it so move on
-            break
-        deps_list = "" # not found, so reset
+            return deps_list
 
     # if no dependency file found throw an error
-    if not deps_list:
-        throw_error('No supported dependency list found in project directory')
-
-    return deps_list
+    throw_error('No supported dependency list found in indicated project directory')
+    return False
 
 def get_data(url):
-    """ Make API request and return the data """
-
+    """ Make API request and return the data (if any) """
     request = requests.get(url)
     if request.status_code != 200:
         throw_error('API request failed: {}'.format(url))
-
     return request
 
 def get_upstream(package):
@@ -104,19 +108,21 @@ def process_cves(cves):
 
     # check for CVEs
     length = len(cves)
-    if length == 0:
-        print(GREEN + "No CVEs found")
+    if length == 0: # no CVEs
+        report(GREEN + "No CVEs found")
     else: # CVEs found so output data
 
         PARAMS['vulnerable'] += 1
-        print(RED + "{} CVE{} FOUND".format(length, "" if length == 1 else "s"))
+        report(RED + "{} CVE{} FOUND".format(length, "" if length == 1 else "s"))
+
+        # if verbose flag was passed output CVE details
         if PARAMS['verbose']:
             for count, cve in enumerate(cves):
 
                 # output CVE data
-                print("\n{}. {}".format(count+1, cve['id']))
-                print("Description: {}".format(cve['summary']))
-                print("Link: https://www.cvedetails.com/cve/{}/".format(cve['id']))
+                report("\n{}. {}".format(count+1, cve['id']))
+                report("Description: {}".format(cve['summary']))
+                report("Link: https://www.cvedetails.com/cve/{}/".format(cve['id']))
 
                 # assign severity (based on score)
                 score = float(cve['cvss'])
@@ -126,7 +132,7 @@ def process_cves(cves):
                     severity = "medium"
                 elif score >= 7.0:
                     severity = RED + "high"
-                print("Severity: {} ({})".format(severity, cve['cvss']))
+                report("Severity: {} ({})".format(severity, cve['cvss']))
 
 def process_dependencies(deps_list):
     """ Process dependencies from project list """
@@ -151,15 +157,15 @@ def process_dependencies(deps_list):
                     version = data[1]
                 except IndexError: # no version provided so check for any CVEs
                     version = "*"
-                print(Style.BRIGHT + "\n{} ({})".format(package, version))
+                report(Style.BRIGHT + "\n{} ({})".format(package, version))
 
                 # gather current stable upstream version
                 upstream = get_upstream(package)
                 if upstream > version and version != "*":
                     PARAMS['outdated'] += 1
-                    print(YELLOW + "OUT OF DATE - Current version is {}".format(upstream))
+                    report(YELLOW + "OUT OF DATE - Current version is {}".format(upstream))
                 else:
-                    print(GREEN + "Version is up to date")
+                    report(GREEN + "Version is up to date")
 
                 # get CVE data (if any)
                 cve_url = "https://cve.circl.lu/api/cvefor/cpe:2.3:a:python:"
@@ -174,11 +180,11 @@ def output_summary(field):
     found = PARAMS[field]
     if found:
         color = YELLOW if field == "outdated" else RED
-        suffix = "y" if found == 1 else "ies"
+        suffix = make_suffix(field)
         percent = int(found * 100 / PARAMS['total'])
-        print(color + '{} {} dependenc{} ({}%)'.format(found, field, suffix, percent))
+        report(color + '{} {} dependenc{} ({}%)'.format(found, field, suffix, percent))
     else:
-        print(GREEN + "0 {} dependencies".format(field))
+        report(GREEN + "0 {} dependencies".format(field))
 
 #####################################################
 # MAIN MODULE
@@ -194,16 +200,14 @@ def __main__():
     parser.add_argument('--version', '-V', action="version", version='%(prog)s 0.1')
     parser.add_argument('--check', '-c', action="store_true", help="Run %(prog)s on itself")
 
-    # parse user-provided input
+    # parse user-provided input and assign
     args = parser.parse_args()
     PARAMS['verbose'] = args.verbose
-    PARAMS['boolean'] = args.boolean # fixthis >> add
-
-    # start scan
-    print("\nStarting {}...".format("self-check" if args.check else "scan"))
+    PARAMS['boolean'] = args.boolean
 
     # confirm target path
     path = confirm_project_path(args)
+    report("\nStarting {}...".format("scan" if args.path else "self-check"))
 
     # find dependency list within project directory
     deps_list = find_dependencies_list(path)
@@ -212,11 +216,17 @@ def __main__():
     process_dependencies(deps_list)
 
     # compile summary
-    print(Style.BRIGHT + '\nSUMMARY')
-    print("{} dependenc{} scanned".format(PARAMS['total'], "y" if PARAMS['total'] == 1 else "ies"))
-    output_summary('outdated')
-    output_summary('vulnerable')
-    print('\n')
+    if PARAMS['boolean']: # return pass / fail
+        if PARAMS['outdated'] or PARAMS['vulnerable']: # there were issues so fail
+            print("FAIL")
+        else: # no issues so pass
+            print("PASS")
+    else: # return summary
+        report(Style.BRIGHT + '\nSUMMARY')
+        report("{} dependenc{} scanned".format(PARAMS['total'], make_suffix('total')))
+        output_summary('outdated')
+        output_summary('vulnerable')
+        print('')
 
 if __name__ == '__main__':
     __main__()
